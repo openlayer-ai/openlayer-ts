1e55ec907f6d62ba37d8a5a18ed9567d0f8110cd
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2024-11-21 10:42:27 -0300

    feat(examples): add OpenAI and LangChain examples using tracing

---
+++ examples/multi-step-tracing.ts
@@ -0,0 +1,40 @@
+import OpenAI from 'openai';
+import traceOpenAI from 'openlayer/src/lib/integrations/openai-tracer';
+import trace from 'openlayer/src/lib/tracing/tracer';
+
+// First, make sure you export your:
+// - OPENAI_API_KEY -- or the API key of the model you're using
+// - OPENLAYER_API_KEY
+// - OPENLAYER_INFERENCE_PIPELINE_ID
+// as environment variables.
+
+// Then, wrap the OpenAI client with Openlayer's traceOpenAI
+const client = traceOpenAI(new OpenAI() as any);
+
+// Wrap each function you would like to trace with Openlayer's trace
+const tracedPreparePrompt = trace(function preparePrompt(input: string): { role: string; content: string }[] {
+  return [
+    { role: 'system', content: 'You are a helpful assistant.' },
+    { role: 'user', content: input },
+  ];
+});
+
+const tracedLlmCall = trace(async function llmCall(messages: any): Promise<string | null> {
+  const chatCompletion = await client.chat.completions.create({
+    messages: messages,
+    model: 'gpt-3.5-turbo',
+    stream: false,
+  });
+  const answer = chatCompletion?.choices[0]?.message?.content ?? null;
+  return answer;
+});
+
+// Main function that calls the other traced functions
+const tracedMain = trace(async function main(input: string): Promise<string> {
+  const messages = await tracedPreparePrompt(input);
+  const result = await tracedLlmCall(messages);
+  return result;
+});
+
+// Run the main function
+tracedMain('Are you an AI or an actual human?').catch(console.error);
---
+++ examples/openai-tracing.ts
@@ -0,0 +1,23 @@
+import OpenAI from 'openai';
+import traceOpenAI from 'openlayer/src/lib/integrations/openai-tracer';
+
+// First, make sure you export your:
+// - OPENAI_API_KEY
+// - OPENLAYER_API_KEY
+// - OPENLAYER_INFERENCE_PIPELINE_ID
+// as environment variables.
+
+// Then, wrap the OpenAI client with Openlayer's traceOpenAI
+const client = traceOpenAI(new OpenAI() as any);
+
+// Finally, use the wrapped OpenAI client normally.
+// The trace is being streamed to your Openlayer inference pipeline.
+async function main(input: string): Promise<string | null> {
+  const response = await client.chat.completions.create({
+    model: 'gpt-4o',
+    messages: [{ role: 'user', content: input }],
+  });
+  return response?.choices[0]?.message?.content ?? null;
+}
+
+main('How are you doing today?').catch(console.error);
